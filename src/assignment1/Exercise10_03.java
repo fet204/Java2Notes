c920a15cb8113590ee9dffdd09665362d7ea2a5b
FrankT <306933@gmail.com>
2019-04-04 10:44:09 -0700

    switched fields to just be public
    just public

--- src/assignment1/Exercise10_03.java
+++ src/assignment1/Exercise10_03.java
@@ -2,7 +2,9 @@ package assignment1;
 
 public class Exercise10_03 {
 	public static void main(String[] args) {
+		
 		MyInteger n1 = new MyInteger(5);
+		
 		System.out.println("n1 is even? " + n1.isEven());
 		System.out.println("n1 is prime? " + n1.isPrime());
 		System.out.println("n1 is odd? " + n1.isOdd());		
@@ -13,52 +15,61 @@ public class Exercise10_03 {
 
 class MyInteger {
 	
-	static int value; //the data field that is in the requirements.
+	private int value; //the data field that is in the requirements.
 	static boolean even = false;
 	static boolean prime = false;
 	static boolean odd = false;
 
-	/*
 	public MyInteger(int x) {
 		this.value = value;
 	}
-	*/
 	
-	public int getX() {
+	public int getVal() {
 		return value;
 	}
 	
-	public static boolean isEven() {
-		
+	// CONDITIONS TO DETERMINE IF ITS EVEN ODD OR PRIME
+	public boolean isEven() {
 		if (value % 2 == 0) {
 			even = true;
 		}
-		
 		return even;
 	}
 	
-	public static boolean isOdd() {
-		
+	public boolean isOdd() {
 		if (value % 2 != 0) {
 			odd = true;
 		}
-			
 		return odd;
 	}
 	
-	public static boolean isPrime() {
-		
+	public boolean isPrime() {
 		for (int i = 0; i > ((value / 2) + 1); i++) {
 			if (value % i == 0) {
 				prime = true;
 			}
 		}
-		
 		return prime;
 	}
-	
-	
-	
-	
-	
+		
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
