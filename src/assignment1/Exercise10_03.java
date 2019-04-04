e434667dc52a6c401827226e9dd6c95aec92ebe9
FrankT <306933@gmail.com>
2019-04-04 10:19:25 -0700

    fixing code
    fixing code

--- src/assignment1/Exercise10_03.java
+++ src/assignment1/Exercise10_03.java
@@ -3,45 +3,32 @@ package assignment1;
 public class Exercise10_03 {
 	public static void main(String[] args) {
 		MyInteger n1 = new MyInteger(5);
-		System.out.println("n1 is even?" + n1.isEven());
-		System.out.println("n1 is prime?" + n1.isPrime());
-		System.out.println("n1 is odd?" + n1.isOdd());		
+		System.out.println("n1 is even? " + n1.isEven());
+		System.out.println("n1 is prime? " + n1.isPrime());
+		System.out.println("n1 is odd? " + n1.isOdd());		
 		
-	}//main
+	}
 
-}//exercise
+}
 
 class MyInteger {
 	
-	//Write code below
-	int value; //the data field that is in the requirements.
-	boolean even = false;
-	boolean prime = false;
-	boolean odd = false;
+	static int value; //the data field that is in the requirements.
+	static boolean even = false;
+	static boolean prime = false;
+	static boolean odd = false;
 
+	/*
 	public MyInteger(int x) {
 		this.value = value;
-		
-		if (value % 2 == 0) {
-			even = true;
-		}
-		else {
-			odd = true;
-		}
-		
-		for (int i = 0; i > ((value / 2) + 1); i++) {
-			if (value % i == 0) {
-				prime = true;
-			}
-		}
-		
 	}
+	*/
 	
 	public int getX() {
 		return value;
 	}
 	
-	public boolean isEven(int x) {
+	public static boolean isEven() {
 		
 		if (value % 2 == 0) {
 			even = true;
@@ -50,6 +37,25 @@ class MyInteger {
 		return even;
 	}
 	
+	public static boolean isOdd() {
+		
+		if (value % 2 != 0) {
+			odd = true;
+		}
+			
+		return odd;
+	}
+	
+	public static boolean isPrime() {
+		
+		for (int i = 0; i > ((value / 2) + 1); i++) {
+			if (value % i == 0) {
+				prime = true;
+			}
+		}
+		
+		return prime;
+	}
 	
 	
 	
