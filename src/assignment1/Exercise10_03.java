d07ef8a42be91276c746095b6ad8a338eaf61d33
FrankT <306933@gmail.com>
2019-04-04 15:22:24 -0700

    fixed by vince

--- src/assignment1/Exercise10_03.java
+++ src/assignment1/Exercise10_03.java
@@ -3,7 +3,7 @@ package assignment1;
 public class Exercise10_03 {
 	public static void main(String[] args) {
 		
-		MyInteger n1 = new MyInteger(5);
+		MyInteger n1 = new MyInteger(23);
 		
 		System.out.println("n1 is even? " + n1.isEven());
 		System.out.println("n1 is prime? " + n1.isPrime());
@@ -17,7 +17,7 @@ class MyInteger {
 	
 	int value; //the data field that is in the requirements.
 	static boolean even = false;
-	static boolean prime = false;
+	static boolean prime = true;
 	static boolean odd = false;
 
 	public MyInteger(int x) {
@@ -38,19 +38,20 @@ class MyInteger {
 	}
 	
 	public boolean isOdd() {
-		if (value % 2 == 0) {
+		if (value % 2 != 0) {
 			odd = true;
 		}
 		return odd;
 	}
 	
 	public boolean isPrime() {
-		for (int i = 1; i <= value; i++) {
+		for (int i = 2; i < value; i++) {
 			if (value % i == 0) {
-				prime = true;
+				return prime = false;
 			}
+				
 		}
-		return prime;
+		return prime = true;
 	}
 		
 }
