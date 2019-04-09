1e7020411c970e381d6f94259b3d53c62890a66b
FrankT <306933@gmail.com>
2019-04-09 13:11:09 -0700

    made default package
    this holds law and marketer. Marketer calls from law so had to make a law class for marketer to use.

---
+++ Week02SRC/Law.java
@@ -0,0 +1,9 @@
+
+public class Law {
+	private int salary;
+	
+	public int standardSal() {
+		salary = 40000;
+		return salary;
+	}
+}
--- Week02SRC/Marketer.java
+++ Week02SRC/Marketer.java
@@ -1,4 +1,9 @@
 
-public class Marketer {
+public class Marketer extends Law {
+	private int marketerSal;
+	public void getSal () {
+		marketerSal = super.standardSal() + 10000;
+	}
+	
 
 }
