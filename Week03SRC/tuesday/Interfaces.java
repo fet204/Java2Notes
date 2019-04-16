e1f80c3a390b6e2cb94212ce25bf515ac82da2db
FrankT <306933@gmail.com>
2019-04-16 15:14:31 -0700

    class notes
    Notes from tuesday class notes that are typed into the week03src. BTW NEXT WEEK IS QUIZ

--- Week03SRC/tuesday/Interfaces.java
+++ Week03SRC/tuesday/Interfaces.java
@@ -1,5 +1,10 @@
 package tuesday;
 
 public class Interfaces {
-
+	/*
+	 * when you have have a header without a body it is known as an abstract method
+	 * use impliments (just like extend) to use abstract methods.
+	 * when you are using extends it only uses one parent and you can't pull from multiple parents
+	 * 
+	*/
 }
