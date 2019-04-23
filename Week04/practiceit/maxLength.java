2e6a92507c195d0384f2aaaea4f3eaad3e95715f
FrankT <306933@gmail.com>
2019-04-23 16:05:07 -0700

    practice it
    chapt 10 maxLength

--- Week04/practiceit/maxLength.java
+++ Week04/practiceit/maxLength.java
@@ -4,6 +4,21 @@ import java.util.*;
 public class maxLength {
 	public static void main(String[] args) {
 		ArrayList<String> test = new ArrayList<String>();
+	}
+	
+	public static int maxLength(ArrayList<String> list) {
+		
+		int zero = 0;
+		
+		for (int i = 0; i < list.size(); i ++) {
+			String var1 = list.get(i); 
+		}
+		
+		if (var1.length() > zero ) {
+			zero = var1.length();
+		}
+		
+		return zero;
 		
 	}
 
