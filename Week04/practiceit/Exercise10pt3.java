17863648ba9d968792c923b9547033c2dc1450d8
FrankT <44277992+fet204@users.noreply.github.com>
2019-04-25 13:12:56 -0700

    10.3 work
    atempting 10.3

---
+++ Week04/practiceit/Exercise10pt3.java
@@ -0,0 +1,20 @@
+package practiceit;
+
+import java.util.ArrayList;
+
+public class Exercise10pt3 {
+	
+	public static void removeEvenLength(ArrayList<String> list) {
+		for(int i = 0; i < list.size(); i++) {
+			
+			String word = list.get(i);
+			int wordLen = word.length();
+			
+			if (wordLen % 2 == 0) {
+				list.remove(i);
+				
+			}
+		}
+	}
+
+}
