9873584c99251c73ea11c1bc5075e1ddfd1a5844
FrankT <44277992+fet204@users.noreply.github.com>
2019-04-25 12:58:32 -0700

    Working on 10.2 exercise
    Working on code so i can turn in the stuff

---
+++ Week04/practiceit/Exercise10Pt2.java
@@ -0,0 +1,40 @@
+package practiceit;
+import java.util.ArrayList;
+
+public class Exercise10Pt2 {
+	public static void main(String[] args) {
+		ArrayList<String> var1 = new ArrayList<String>();
+		var1.add("to");
+		var1.add("be");
+		var1.add("or");
+		var1.add("not");
+		var1.add("to");
+		var1.add("be");
+		var1.add("hamlet");
+		
+		swapPairs(var1);
+	}
+	
+	public static void swapPairs(ArrayList<String> list) {
+		
+		String oddhold;
+		
+		if (list.size() % 2 != 0) {
+			String oddHold = list.get(list.size());
+			list.remove(list.size());
+			
+			for(int i = 0; i < list.size(); i++) {
+				String first = list.get(i);
+				list.remove(i);
+				String second = list.get(i);
+				list.remove(i);
+			}
+			System.out.println(list);
+			
+		}
+		list.add(oddhold);
+		
+		}
+		
+	}
+
