e9f746a004572ba7b1cc5a6b250ff4e09850c2c3
FrankT <44277992+fet204@users.noreply.github.com>
2019-04-26 12:33:36 -0700

    10.4 finish
    title

--- Week04/practiceit/Exercise10Pt2.java
+++ Week04/practiceit/Exercise10Pt2.java
@@ -3,6 +3,15 @@ import java.util.ArrayList;
 
 public class Exercise10Pt2 {
 	public static void main(String[] args) {
+		/*
+		ArrayList<String> var2 = new ArrayList<String>();
+		var2.add("four");
+		var2.add("score");
+		var2.add("and");
+		var2.add("seven");
+		var2.add("years");
+		var2.add("ago");
+		
 		ArrayList<String> var1 = new ArrayList<String>();
 		var1.add("to");
 		var1.add("be");
@@ -12,28 +21,43 @@ public class Exercise10Pt2 {
 		var1.add("be");
 		var1.add("hamlet");
 		
+
+		swapPairs(var2);
 		swapPairs(var1);
+		*/
 	}
 	
 	public static void swapPairs(ArrayList<String> list) {
 		
-		
-		
 		String oddhold = "";
 		
 		if (list.size() % 2 != 0) {
-			String oddHold = list.get(list.size());
+			String oddHold = list.get(list.size()-1);
 			list.remove(list.size()-1);
 			
-			System.out.println(list);
-			
+			for(int i = 0; i < list.size(); i+=2) {
+				String first = list.get(i);
+				String second = list.get(i+1);
+				
+				list.set(i, second);
+				list.set(i+1, first);
+			}
+			list.add(oddHold);
 		}
-		for(int i = 0; i < list.size(); i++) {
-			String first = list.get(i);
-			list.remove(i);
-			String second = list.get(i);
-			list.remove(i);
+		
+		
+		else { 
+			for(int i = 0; i < list.size(); i+=2) {
+				String first = list.get(i);
+				String second = list.get(i+1);
+				
+				list.set(i, second);
+				list.set(i+1, first);
+
+			}
 		}
+		
+		System.out.println(list);
 		
 		}
 	
--- Week04/practiceit/Exercise10Pt4.java
+++ Week04/practiceit/Exercise10Pt4.java
@@ -1,6 +1,25 @@
 package practiceit;
 
+import java.util.ArrayList;
+
 public class Exercise10Pt4 {
-	public 
+	public static void main(String [] args) {
+		ArrayList<String> var1 = new ArrayList<String>();
+		var1.add("how");
+		var1.add("are");
+		var1.add("you");
+
+		doubleList(var1);
+	}
+	
+	
+	public static void doubleList(ArrayList<String> list) {
+		for(int i = list.size(); i > 0; i--) {
+			String hold = list.get(i-1);
+			list.add(i-1, hold);
+			
+		}
+		System.out.println(list);
+	}
 
 }
