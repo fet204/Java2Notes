5414c993d0b64212b908ac66cbe8dad21e3fab17
FrankT <44277992+fet204@users.noreply.github.com>
2019-05-01 10:44:03 -0700

    recursion
    chapt10

--- Week05/practiceItChapt12/starString.java
+++ Week05/practiceItChapt12/starString.java
@@ -1,18 +1,32 @@
 package practiceItChapt12;
+import java.math.*;
 
 public class starString {
+	
+	public static void main(String[] args) {
+		starString(2);
+	}
+	
 	public static String starString(int value){
+		
+		int base = 2;
+		int totStar;
+		
 		if (value == 0) {
 			System.out.println("*");
 		}
 		
-		else if(value <= 0) {
-			starString(value - 1);
-			System.out.println("*" * 2**value);
+		else if (value == 1) {
+			System.out.println("**");
 		}
 		
-		return
+		else {
+			System.out.println("*");
+			totStar = (int) Math.pow(base, value);
+			starString(value - 1);
+		}
 		
+		return "return";
 	}
 
 }
