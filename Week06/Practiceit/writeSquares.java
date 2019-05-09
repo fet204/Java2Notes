cb1e3f7c2c613fc0d776adac688e244f66e19ec1
FrankT <44277992+fet204@users.noreply.github.com>
2019-05-09 10:07:18 -0700

    exercise12.7
    writeSquares debugging and starting writeChars

--- Week06/Practiceit/writeSquares.java
+++ Week06/Practiceit/writeSquares.java
@@ -1,27 +1,35 @@
-package Practiceit;
+package practiceit;
 
 public class writeSquares {
+	
 	public static void main(String[] args) {
-		writeSquares(5);
+		writeSquares(8);
 	}
 	
 	public static void writeSquares(int n) {
-		if (n < 1) {
+		if (n < 1)  {
 			throw new IllegalArgumentException();
 		}
 		
-		else if (n == 1) {
-			System.out.println("1");
+		if (n == 1) {
+			System.out.print("1");
 		}
 		
 		else {
 			if (n % 2 != 0) {
-				System.out.print(n * n);
+				System.out.print(n * n + ", ");
+				writeSquares(n - 2);
+				System.out.print(", " + (n - 1) * (n - 1));
+				//System.out.print();
 			}
 			else {
-				System.out.print(n * n);
+				System.out.print((n - 1) * (n - 1) + ", ");
+				writeSquares(n - 2);
+				System.out.print(", " + n * n);
+				//System.out.print(", ");
 			}
 		}
+		
 	}
 
 }
