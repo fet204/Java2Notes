a970511191d637d12cf81c3470123def28481adc
FrankT <44277992+fet204@users.noreply.github.com>
2019-05-09 13:03:34 -0700

    fixing error
    error: could not find or load main class testPackage.Java2FinalTest. For some reason all the classes cannot run anymore.

--- Java2Final/testPackage/Java2FinalTest.java
+++ Java2Final/testPackage/Java2FinalTest.java
@@ -1,5 +1,8 @@
 package testPackage;
 
 public class Java2FinalTest {
+	public static void main(String[] args) {
+		System.out.println("Holla");
+	}
 
 }
---
+++ Week06/tuesday/test.java
@@ -0,0 +1,8 @@
+package tuesday;
+
+public class test {
+	public static void main(String[] args) {
+		System.out.println(5%10);
+	}
+
+}
